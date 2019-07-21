78122a282aa2543b958039a859cfa6d2607e4eec
Joel Raju <joelraju@ymail.com>
2019-07-22 01:43:14 +0530

    Bootstrapped a Gatsby blog

---
+++ config.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Blog by Joel Raju',
+  subtitle: 'Hey there ! I\'m a softwere engineer who makes stuff. Come say hi!',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Joel Raju',
+    photo: '/photo.jpg',
+    bio: 'Hey there ! I\'m a softwere engineer who makes stuff. Come say hi!',
+    contacts: {
+      email: '#',
+      telegram: '#',
+      twitter: '#',
+      github: 'https://github.com/Joel-Raju',
+      rss: '#',
+      vkontakte: '#'
+    }
+  }
+};
