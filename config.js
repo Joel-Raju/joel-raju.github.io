b375123a405c15305310b3b5b635a79b645a3476
Joel Raju <Joel-Raju@users.noreply.github.com>
2022-05-09 20:28:08 +0530

    fixed typo

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: 'https://joelraju.com',
   pathPrefix: '/',
   title: 'Blog by Joel Raju',
-  subtitle: "Hey there ! I'm a softwere engineer and an indie maker.",
+  subtitle: "Hey there ! I'm a software engineer and an indie maker.",
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 25,
@@ -31,7 +31,7 @@ module.exports = {
   author: {
     name: 'Joel Raju',
     photo: '/media/photo.png',
-    bio: "Hey there ! I'm a softwere engineer and an indie maker.",
+    bio: "Hey there ! I'm a software engineer and an indie maker.",
     contacts: {
       github: 'Joel-Raju',
       linkedin: 'joelraju',
