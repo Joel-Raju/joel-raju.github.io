a0eb5205da9a8fe6b1e3c8dbf7a699ba19d57c5e
Joel Raju <Joel-Raju@users.noreply.github.com>
2025-03-10 16:09:36 +0530

    Update use-site-metadata.js

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -14,7 +14,6 @@ const useSiteMetadata = () => {
               contacts {
                 github
                 linkedin
-                twitter
                 medium
                 devto
                 stackoverflow
